fbd4254bf7e1f92e5e9c6e3672f8071e2160b47c
Sanskar Srivastava <sanskarss.2002@gmail.com>
2026-01-23 00:48:01 +0530

    Updated common UI Theme

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -35,7 +35,7 @@ const Header: React.FC<HeaderProps> = ({ onNavigate, currentView }) => {
             className="flex items-center gap-2 group cursor-pointer"
             onClick={() => onNavigate('home')}
           >
-            <div className="w-8 h-8 text-primary">
+            <div className="w-8 h-8 text-moss-green">
               <svg fill="none" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
                 <path d="M36.7273 44C33.9891 44 31.6043 39.8386 30.3636 33.69C29.123 39.8386 26.7382 44 24 44C21.2618 44 18.877 39.8386 17.6364 33.69C16.3957 39.8386 14.0109 44 11.2727 44C7.25611 44 4 35.0457 4 24C4 12.9543 7.25611 4 11.2727 4C14.0109 4 16.3957 8.16144 17.6364 14.31C18.877 8.16144 21.2618 4 24 4C26.7382 4 29.123 8.16144 30.3636 14.31C31.6043 8.16144 33.9891 4 36.7273 4C40.7439 4 44 12.9543 44 24C44 35.0457 40.7439 44 36.7273 44Z" fill="currentColor"></path>
               </svg>
@@ -63,11 +63,11 @@ const Header: React.FC<HeaderProps> = ({ onNavigate, currentView }) => {
           </div>
           <div className="flex gap-4">
             <button className="transition-all duration-200 group">
-              <CircleUser className="text-sage transition-colors group-hover:text-uranium-green" size={ICONS.SIZE.MD} />
+              <CircleUser className="text-sage transition-colors group-hover:text-gold-muted" size={ICONS.SIZE.MD} />
             </button>
             <button className="relative transition-all duration-200 group">
-              <ShoppingCart className="text-sage transition-colors group-hover:text-uranium-green" size={ICONS.SIZE.MD} />
-              <span className="absolute -top-1 -right-1 bg-primary text-white text-[8px] rounded-full w-4 h-4 flex items-center justify-center">2</span>
+              <ShoppingCart className="text-sage transition-colors group-hover:text-gold-muted" size={ICONS.SIZE.MD} />
+              <span className="absolute -top-1 -right-1 bg-moss-green text-white text-[8px] rounded-full w-4 h-4 flex items-center justify-center">2</span>
             </button>
           </div>
         </div>
